fad4e3588320fad0570b1c653ccc350da06b3e93
Saif Sajid <saif@saifsajid.com>
2026-06-04 22:14:31 -0700

    Add: duplicate-integer - submission-6

---
+++ Data Structures & Algorithms/duplicate-integer/submission-6.py
@@ -0,0 +1,50 @@
+"""
+Requirements:
+
+Ideas:
+
+if len(nums) <= 1:
+    return False
+
+maintain a set, 
+    go through each item, 
+        check if num in set, 
+            if so return true, 
+            if not, add to set
+
+Space - O(n)
+time - O(n)
+
+
+
+
+"""
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        return self.save_space(nums)
+
+    def default(self, nums: List[int]) -> bool:
+        if len(nums) <= 1:
+            return False
+
+        seen = set()
+
+        for num in nums:
+            if num in seen:
+                return True
+            else:
+                seen.add(num)
+        return False
+
+    def save_space(self, nums: List[int]) -> bool:
+        if len(nums) <= 1:
+            return False
+
+        nums.sort()
+
+        for idx, num in enumerate(nums[:-1]):
+            if num == nums[idx + 1]:
+                return True
+
+        return False
